0a3d6cd7112cd2eb00974e2b106e3d67c49528b2
Jonathan Hartman <j@hartman.io>
2024-08-26 14:44:37 -0700

    [firebot] Update to 5.63.1
    Also adds a Rosetta caveat since it still seems to be building for Intel-only.

--- Casks/firebot.rb
+++ Casks/firebot.rb
@@ -1,11 +1,7 @@
 cask "firebot" do
-  version "5.62.1"
-  sha256 "4225fbcf88ef34ad9d92333395e4ef97f48198f3f960536706f5f917bb3b5ce4"
+  version "5.63.1"
+  sha256 "b2977328f2a701a5e934b2d995ce576224ddca0762125d2ece38ea94be36715d"
 
-  # Firebot is currently x64-only but, if I'm reading right, the next release
-  # should get an ARM build by virtue of them using the `macos-latest` GitHub
-  # Actions runner and that having changed to an M1 runner since the last
-  # Firebot release.
   url "https://github.com/crowbartools/Firebot/releases/download/v#{version}/firebot-v#{version}-macos-x64.dmg",
       verified: "github.com/crowbartools/Firebot/"
   name "Firebot"
@@ -25,4 +21,8 @@ cask "firebot" do
     "#{Dir.home}/Library/Preferences/com.electron.firebot.plist",
     "#{Dir.home}/Library/Saved Application State/com.electron.firebot.savedState",
   ]
+
+  caveats do
+    requires_rosetta
+  end
 end
