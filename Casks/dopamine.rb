0c3c173d917ef61eb3268ce251137d0a076aacd4
Jonathan Hartman <j@hartman.io>
2024-10-21 16:13:17 -0700

    [dopamine] Update to 3.0 preview 35

--- Casks/dopamine.rb
+++ Casks/dopamine.rb
@@ -1,6 +1,6 @@
 cask "dopamine" do
-  version "3.0.0-preview.34"
-  sha256 "20f6635bcdf4e96b61eb57c4e9f0abd1c48ddcf3b9a79da77d7056f5965df6d4"
+  version "3.0.0-preview.35"
+  sha256 "b114390bbcf92862c77d4be79dbb8c162e254ac34640d4dc46d45f002909fc35"
 
   url "https://github.com/digimezzo/dopamine/releases/download/v#{version}/Dopamine-#{version}.dmg"
   name "Dopamine"
