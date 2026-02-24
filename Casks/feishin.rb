e953ff1f8bfeb40cc84ef02dacd8301c8f68f06a
Jonathan Hartman <j@hartman.io>
2026-02-24 10:15:35 -0800

    [feishin] Update to 1.6.0

--- Casks/feishin.rb
+++ Casks/feishin.rb
@@ -1,9 +1,9 @@
 cask "feishin" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.4.2"
-  sha256 arm:   "89a1384c572ed7dc4064c80cefcf9bb0a6c6f56e86f53f706e488b535dbb68af",
-         intel: "d3f3989ed84915267d1c6ee1c711735e9a7e7b66bab155edd4458ab794683861"
+  version "1.6.0"
+  sha256 arm:   "01d3fe12d52a6eb4908125954b4a0486770574b3b37984769982d53a70e8b126",
+         intel: "d0894f0a0a91254cc6e4baaea51358948b73d17ff4ac547a8c5570f3100d9c3f"
 
   url "https://github.com/jeffvli/feishin/releases/download/v#{version}/Feishin-#{version}-mac-#{arch}.dmg"
   name "Feishin"
