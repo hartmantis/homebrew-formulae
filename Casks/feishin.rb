0a0768a70fc403273666f2279dad179a74242947
Jonathan Hartman <j@hartman.io>
2026-03-16 09:22:19 -0700

    [feishin] Update to 1.9.0

--- Casks/feishin.rb
+++ Casks/feishin.rb
@@ -1,9 +1,9 @@
 cask "feishin" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.8.0"
-  sha256 arm:   "f4368db7ca88e0e231e2c52d87ed13c4e218380d5a0fa3f62d8557dc9165c600",
-         intel: "dca0a75d8c2f8e7f8706dccce9f2d024587f06643f3c150ee613c885bc0bb066"
+  version "1.9.0"
+  sha256 arm:   "41f83769c666f543b739043f6a5ff4d4418863744d482cef7106df84c8e82cae",
+         intel: "86aed7f9101b994890cbfd17256d80a84f6893003ac336d375b296c59ad95c5b"
 
   url "https://github.com/jeffvli/feishin/releases/download/v#{version}/Feishin-#{version}-mac-#{arch}.dmg"
   name "Feishin"
